163a278b3d4e438f86477cca5d7f2188343b97e2
parinthon kongthong <parinthonkongthong@gmail.com>
2026-09-11 16:01:24 +0700

    Update: Finally filter student in internship pages!!!!

--- frontend/app/internship/[id]/page.tsx
+++ frontend/app/internship/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { Internships } from "@/types/internship";
+import { Students } from "@/types/student";
 import { useParams } from "next/navigation";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
@@ -8,6 +9,7 @@ import Link from "next/link";
 export default function InternshipPage() {
   const { id } = useParams();
   const [internships, setInternships] = useState<Internships[]>([]);
+  const [students, setStudents] = useState<Students[]>([]);
 
   useEffect(() => {
     async function getInternship() {
@@ -18,8 +20,23 @@ export default function InternshipPage() {
     getInternship();
   }, []);
 
-  const filtered = internships.filter(
+  useEffect(() => {
+    async function getStudent() {
+      const response = await fetch("http://localhost:8000/students");
+      setStudents(await response.json());
+    }
+
+    getStudent();
+  }, []);
+
+  const internFiltered = internships.filter(
     (internship) => internship.company_id === Number(id),
+  );
+
+  const studentIds = internFiltered.map((item) => item.student_id);
+
+  const studentFiltered = students.filter((student) =>
+    studentIds.includes(student.student_id),
   );
 
   return (
@@ -34,20 +51,26 @@ export default function InternshipPage() {
         <thead>
           <tr>
             <th>ID</th>
-            <th>Student_ID</th>
-            <th>Title</th>
-            <th>Company_ID</th>
-            <th>Description</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Image</th>
+            <th>Role</th>
+            <th>Lineage</th>
+            <th>Contact</th>
+            <th>Instagram</th>
           </tr>
         </thead>
         <tbody>
-          {filtered.map((internship) => (
-            <tr key={internship.internship_id}>
-              <td>{internship.internship_id}</td>
-              <td>{internship.student_id}</td>
-              <td>{internship.internship_title}</td>
-              <td>{internship.company_id}</td>
-              <td>{internship.internship_description}</td>
+          {studentFiltered.map((student) => (
+            <tr key={student.student_id}>
+              <td>{student.student_id}</td>
+              <td>{student.student_firstname}</td>
+              <td>{student.student_lastname}</td>
+              <td>{student.student_image}</td>
+              <td>{student.student_role}</td>
+              <td>{student.student_lineage}</td>
+              <td>{student.student_contact}</td>
+              <td>{student.student_instagram}</td>
             </tr>
           ))}
         </tbody>
